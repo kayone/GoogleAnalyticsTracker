23f9f1e81f9aab3270bc02bcfe6904d3e1851add
Keivan Beigi <kay.one@gmail.com>
2012-05-04 16:42:35 -0700

    Console harness, should prob be replaced with unit tests.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using GoogleAnalyticsTracker;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var tracker = new Tracker("UA-8318723-11", "exceptrack.com");
+
+            for (int i = 0; i < 1000; i++)
+            {
+                tracker.TrackPageView("GA API No Session", "GaapiNosession");
+                tracker.TrackEvent("AAAAA", "MyAction","L1", "V1");
+                Thread.Sleep(500);
+            }
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
